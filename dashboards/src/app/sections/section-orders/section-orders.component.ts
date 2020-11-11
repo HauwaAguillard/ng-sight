2c8e8547c7e8f8cf611b93215d5f206be58026f1
HauwaAguillard <hauwa.aguillard@revelry.co>
2020-11-11 10:25:55 -0600

    updates to orders

--- dashboards/src/app/sections/section-orders/section-orders.component.ts
+++ dashboards/src/app/sections/section-orders/section-orders.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from '../../shared/order';
 
 @Component({
   selector: 'app-section-orders',
@@ -8,6 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  public orders: Order[] = [
+    {id: 1, customer: 
+      {id: 1, name: 'Jeni Fan', state: 'AR', email: 'jen@fan.com'}, 
+      total: 400, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 4),},
+    {id: 2, 
+      customer: {id: 1, name: 'Jeni Fan', state: 'AR', email: 'jen@fan.com'}, 
+      total: 400, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 4),},
+    {id: 3, 
+      customer: {id: 1, name: 'Jeni Fan', state: 'AR', email: 'jen@fan.com'}, 
+      total: 400, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 4),},
+    {id: 4, 
+      customer: {id: 1, name: 'Jeni Fan', state: 'AR', email: 'jen@fan.com'}, 
+      total: 400, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 4),},
+    {id: 5, 
+      customer: {id: 1, name: 'Jeni Fan', state: 'AR', email: 'jen@fan.com'}, 
+      total: 400, placed: new Date(2017, 12, 1), fulfilled: new Date(2017, 12, 4),},
+  ];
 
   ngOnInit(): void {
   }
